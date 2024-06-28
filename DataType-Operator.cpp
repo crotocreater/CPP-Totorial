3f56b1fe66fcb0cdeb8c725f89cd1bd4ce083ee0
Hoang <casiolo2k5@gmail.com>
2024-06-29 01:44:04 +0700

    fist lesson

---
+++ DataType-Operator.cpp
@@ -0,0 +1,106 @@
+#include<iostream>
+
+using namespace std;
+int main(){
+//	int x = 3;
+//	char a = 'a';
+//	char b = '2';
+//	char c = '*';
+//	
+//	// bool boolean = true / false 
+//	bool isB = false;
+//	bool isA = true;
+//	// 1 = true 
+//	// 0 = false 
+//	cout<< "isB: " << isB << endl;
+//	cout<< "isA: " << isA<< endl;
+//	
+//	
+	
+	// if else 
+//	if {
+//	}else {
+//		if{
+//			
+//		}else{
+//			if{
+//			}else{
+//				
+//			}
+//		}
+////	}
+//	int age;
+//	std::cout<< "nhap tuoi cua ban: ";
+//	std::cin >> age;
+//	
+//	if(age > 18){
+//		std::cout<< "ban du 18 tuoi";
+//	} else if ( age < 0){
+//		std::cout<< "ban nhap ko dung";
+//	}else {
+//		std::cout << "ban chua du 18 tuoi";
+//	}
+	
+//	// kiem tra thu trong tuan 
+//	int isDay;
+//	cout<< "nhap ngay: ";
+//	cin >> isDay;
+//	// trong c++ n� c� 2 tu khoa ch�nh g�m : break, continue, 
+//	switch(isDay){
+//		case 2: cout<< "ngay dau tuan" << '\n'; break;
+//		case 3: cout << "ngay thu 3 " << endl;break;
+//		case 4: cout << "ngay thu 4" << endl;break;
+//		default: cout<< "is default";
+//	}
+	
+//	operator
+//int a  = 10;
+//cout<< a-- << '\n';
+//cout<< --a;
+
+// t�nh t�ng
+//
+//int a, b;
+//cin >> a >> b;
+//cout<< (char) a/b << endl;
+//cout<< a%b;
+
+// to�n tu logic 
+/// gom 3 to�n tu chinh 
+// - && : to�n tu v�
+// ||: hoac 
+// ! : phu dinh ( chi hoat dong voi kieu gia tri boolean
+
+//i
+
+
+
+
+
+// toan tu quan he : > < == <= >= != 
+
+
+int age = 18;
+if(age != 18){
+	cout<< "dung";
+}
+int a = 15, b= 7;
+
+
+// to�n tu gan: +=, -=, *=, /=, %=;
+
+// += => a+=b => a = a+ b;
+// -= => a-=b => a = a-b;
+// *= => a*=b => a = a*b;
+
+
+// a-b
+
+int c =  a > b ?a - b : b - a;
+
+cout<< c;
+
+
+
+//	cout<< a << " " << b << " " << c;
+}
